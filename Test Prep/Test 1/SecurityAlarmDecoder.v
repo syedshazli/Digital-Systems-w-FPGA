6da14d30dca2590aa04c49cf130672ae3c47b9ca
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-01-26 23:25:18 -0500

    Update SecurityAlarmDecoder.v

--- Test Prep/Test 1/SecurityAlarmDecoder.v
+++ Test Prep/Test 1/SecurityAlarmDecoder.v
@@ -1,1 +1,17 @@
+module encoder(
+  input [2:0] D_In,
+  output reg [1:0] D_Out
+)
 
+  
+  always @(*)
+    begin
+      case (D_in):
+        3'101: D_Out = 2'b10;
+        3'110: D_out = 2'b01;
+        3'011: D_out = 2'b11;
+        3'000: D_out = 2'b11;
+        default: D_out = 2'b00;
+      endcase
+    end
+endmodule
