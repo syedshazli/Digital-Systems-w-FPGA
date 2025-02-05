2b403470b9d42bc1a62bde6d2e469f2eb5dbcac7
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-02-04 21:58:43 -0500

    Update top_lab2.v

--- Lab 2/top_lab2.v
+++ Lab 2/top_lab2.v
@@ -1,63 +1,35 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: WPI
-// Engineer: Syed Shazli
-// 
-// Create Date: 01/30/2025 02:36:30 PM
-// Design Name: top_lab2 top file
-// Module Name: top_lab2
-// Project Name: Lab2_ECE_3829
-// Target Devices: Basys3 FPGA
-// Description: This is the top level module for lab 2 for the FPGA lab
-// 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
+## Clock signal
+set_property -dict { PACKAGE_PIN W5   IOSTANDARD LVCMOS33 } [get_ports clk]
+create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports clk]
+
+# Buttons
+set_property -dict { PACKAGE_PIN U18   IOSTANDARD LVCMOS33 } [get_ports btnC]
+
+#7 Segment Display
+set_property -dict { PACKAGE_PIN W7   IOSTANDARD LVCMOS33 } [get_ports {seg[0]}]
+set_property -dict { PACKAGE_PIN W6   IOSTANDARD LVCMOS33 } [get_ports {seg[1]}]
+set_property -dict { PACKAGE_PIN U8   IOSTANDARD LVCMOS33 } [get_ports {seg[2]}]
+set_property -dict { PACKAGE_PIN V8   IOSTANDARD LVCMOS33 } [get_ports {seg[3]}]
+set_property -dict { PACKAGE_PIN U5   IOSTANDARD LVCMOS33 } [get_ports {seg[4]}]
+set_property -dict { PACKAGE_PIN V5   IOSTANDARD LVCMOS33 } [get_ports {seg[5]}]
+set_property -dict { PACKAGE_PIN U7   IOSTANDARD LVCMOS33 } [get_ports {seg[6]}]
 
 
-module top_lab2(
-    input clk, // input clock
-    input btnC, // actuve low negedge, pass down to reset
-    input [1:0] sw, // negedge
-    output wire [6:0] seg,       // Seven-segment display segments
-    output wire [3:0] an         // Anodes for the seven-segment display
-    );
-    // internal wires A-D for sev seg display
-    wire [3:0] dispAWire;
-    wire [3:0] dispBWire; 
-    wire [3:0] dispCWire;
-    wire [3:0] dispDWire;
-    
-    wire hCount;
-    wire vCount;
-    wire blank; // default in all cases
-    wire reset_n;
-    wire clk_25mhz; // 25 mhz output clock
-    
-    // convert 100 mhz input clock to 25 mhz output clock
-    clk_wiz_0 clk_wiz_0i(
-    // Clock out ports
-    .clk_25mhz(clk_25mhz),     // output clk_25mhz
-    
-    // Status and control signals
-    .reset(btnC), // A push button connects to the reset input 
-    .locked(reset_n),       // output locked
-   // Clock in ports
-    .clk_in1(clk)     // input clk_in1
-    ); // end of conversion
-    
-    // seven seg module
-    seven_seg sevseg(
-        .displayA(4'd2),
-        .displayB(4'd7),
-        .displayC(4'd8),
-        .displayD(4'd9),
-        .reset_n(reset_n),
-        .clk_25mhz(clk_25mhz),
-        .segment(segment),
-        .anode(anode)
-    );
-endmodule
+
+set_property -dict { PACKAGE_PIN U2   IOSTANDARD LVCMOS33 } [get_ports {an[0]}]
+set_property -dict { PACKAGE_PIN U4   IOSTANDARD LVCMOS33 } [get_ports {an[1]}]
+set_property -dict { PACKAGE_PIN V4   IOSTANDARD LVCMOS33 } [get_ports {an[2]}]
+set_property -dict { PACKAGE_PIN W4   IOSTANDARD LVCMOS33 } [get_ports {an[3]}]
+
+#Buttons
+
+#set_property -dict { PACKAGE_PIN T18   IOSTANDARD LVCMOS33 } [get_ports btnU]
+#set_property -dict { PACKAGE_PIN W19   IOSTANDARD LVCMOS33 } [get_ports btnL]
+#set_property -dict { PACKAGE_PIN T17   IOSTANDARD LVCMOS33 } [get_ports btnR]
+#set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports btnD]
+set_property -dict { PACKAGE_PIN U18   IOSTANDARD LVCMOS33 } [get_ports btnC]
+
+## Configuration options, can be used for all designs
+set_property CONFIG_VOLTAGE 3.3 [current_design]
+set_property CFGBVS VCCO [current_design]
